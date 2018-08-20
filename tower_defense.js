7598a9c64f350a5600e8d717252f1f19ddbcc3ed
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-08-20 10:20:37 -0700

    create grid

---
+++ tower_defense.js
@@ -0,0 +1,15 @@
+const Grid = require('./lib/grid.js');
+const Unit = require('./lib/unit.js');
+
+window.unit=Unit;
+window.grid=Grid;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("game-canvas");
+  const ctx = canvas.getContext("2d");
+
+
+  
+
+
+});
